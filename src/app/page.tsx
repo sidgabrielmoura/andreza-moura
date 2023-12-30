51066d4bc5f79710491b6644104a7691ba0dae5a
sidgabrielmoura <sidgabrielmoura40@gmail.com>
2023-12-30 19:55:03 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <>
       <main className="w-full h-[100vh] bg-[#323232] flex flex-col items-center justify-center gap-6">
         
-        <h1 className="text-[35px] text-zinc-200 font-extralight" id="nome-home">Andreza Moura...</h1>
+        <h1 className="text-[35px] text-zinc-200 font-extralight" id="nome-home">Andreza Moura</h1>
         <Link href={''} className="bg-[#deff16] w-[150px] h-[40px] rounded-full flex items-center justify-center" id="botao-navegar">Navegar</Link>
         
       </main>
